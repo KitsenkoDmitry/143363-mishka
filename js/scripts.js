90cd3d1b76dad910084de4ec780d41aa2183c19d
Dmitry Kitsenko <kitsenkodv@gmail.com>
2017-03-24 22:34:54 +0300

    доделал адаптивную сетку index и catalog

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ var navMain = document.querySelector(".main-nav");
 var navToogle = document.querySelector(".main-nav__toogle");
 var pageHeader = document.querySelector(".page-header");
 
+var catalogModal = document.querySelector(".catalog-item__modal");
+var productBtn = document.querySelector(".btn--product");
+
 pageHeader.classList.remove("page-header--nojs");
 navMain.classList.remove("main-nav--nojs");
 
@@ -12,5 +15,16 @@ navToogle.addEventListener("click", function() {
   } else {
     navMain.classList.remove("main-nav--opened");
     navMain.classList.add("main-nav--closed");
+  }
+});
+
+productBtn.addEventListener("click", function() {
+  event.preventDefault();
+  if (catalogModal.classList.contains("catalog-item__modal--closed")) {
+    catalogModal.classList.remove("catalog-item__modal--closed");
+    catalogModal.classList.add("catalog-item__modal--opened");
+  } else {
+    catalogModal.classList.remove("catalog-item__modal--opened");
+    catalogModal.classList.add("catalog-item__modal--closed");
   }
 });
